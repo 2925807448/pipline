cd6281b4a72b75fb8a2212ecc3f6655c08927ecf
2925807448 <2925807448@qq.com>
2019-06-02 12:44:06 +0800

    add declarative.Jenkinsfile

---
+++ src/autotest.groovy
@@ -0,0 +1,35 @@
+node {
+    
+    try {
+        checkout scm
+        stage('Build') {
+            echo 'Building....'
+        }
+        stage('Test') {
+            echo 'Building....'
+        }
+        stage('Deploy') {
+            echo 'Deploying....'
+            sh './home/script/deploy.sh'
+        } 
+    } finally {
+        post {
+        always {
+            echo 'One way or another, I have finished'
+            /* clean up our workspace */
+        }
+        success {
+            echo 'I succeeeded!'
+        }
+        unstable {
+            echo 'I am unstable :/'
+        }
+        failure {
+            echo 'I failed :('
+        }
+        changed {
+            echo 'Things were different before...'
+        }
+    }
+    } 
+}
